71cc5d9a1b4db3a1195dc326565df47d0028c55e
wame11 <ethansamuelross@gmail.com>
2026-08-11 19:26:20 +0100

    Update cache name from route66-v10 to route66-v11

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'route66-v10';
+const CACHE_NAME = 'route66-v11';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./stops.js'];
 self.addEventListener('install', (e) => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((names) => Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))).then(() => self.clients.claim())); });
